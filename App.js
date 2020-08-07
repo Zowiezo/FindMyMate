193fbb4043607655a7467fa426a33e4451196109
Zowie <zrnkubheka@gmail.com>
2020-08-07 05:33:42 +0200

    edited files

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
-import MapScreen from './screens/MapScreen';
-import MapListScreen from '.screens/MapListScreen';
-import { setNavigator } from './screens/navigationRef';
-import UserToMeet from './components/UserToMeet';
+// import MapScreen from './screens/MapScreen';
+// import MapListScreen from './screens/MapListScreen';
+
+// import UserToMeet from './components/UserToMeet';
 
 export default class App extends Component {
   render() {
@@ -12,20 +12,61 @@ export default class App extends Component {
     <MapView
       style={{...StyleSheet.absoluteFillObject}}
       initialRegion={{
-        latitude: 33.7872131,
-        longitude: -84.381931,
+        latitude: -26.1874,
+        longitude: 28.0938,
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}>
-      <Marker
-        coordinate={{latitude: 33.7872131, longitude: -84.381931}}
-        title="Flatiron School Atlanta"
-        description="This is where the magic happens!"
-      />
-    </MapView>
+        
+    <Marker
+        coordinate={{latitude: -26.1874, longitude: 28.0938,}}
+        title="Your current location"
+        description="Lets meet up"
+    />
+
+    <Marker 
+        coordinate={{
+        latitude: -26.1863,
+        longitude: 28.0958,}}
+        title="Stacy"
+        description="Loves pets, jogging and Pizza"
+    />    
+
+    <Marker 
+        coordinate={{
+        latitude: -26.1889,
+        longitude: 28.0926,
+      }}
+        title="Mary"
+        description="Loves petss and pizza"
+    />
+
+    <Marker 
+        coordinate={{
+        latitude: -26.1861,
+        longitude: 28.0939,
+      }}
+        title="Ty"
+        description="Loves jogging"
+    />
+
+    <Marker 
+        coordinate={{
+        latitude: -26.1873,
+        longitude: 28.0953,
+       }}
+        title="Peter"
+        description=" Loves pizze and pets"
+    />   
+
+    <Marker 
+        coordinate={{
+        latitude: -26.1889,
+        longitude: 28.0970,
+      }}
+        title="John"
+        description="Loves Jogging" 
+      />           
+        </MapView>
     )
-  }
-
-  
-
-}
+  }}
